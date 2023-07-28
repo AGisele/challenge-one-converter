40bb14971ed5686f331b3c8e64eb8e70ea831465
Gisele Araújo <araujocgisele@gmail.com>
2023-07-28 14:14:52 -0300

    Temperature converter and dialog box - proceed?

--- src/application/DialogBox.java
+++ src/application/DialogBox.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.Locale;
+
 import javax.swing.JOptionPane;
 
 public class DialogBox {
-	
+		
 	public static void menu() {
 
 		String[] options = { "Conversor de Moeda", "Conversor de Temperatura" };
@@ -32,10 +34,55 @@ public class DialogBox {
 				.toString();
 	}
 
-	public static void temperatureConverterMenu() {
+	public static void temperatureConverterMenu() {		
 
 		String[] temperatures = { "De Celsius a Fahrenheit", "De Celsius a Kelvin", "De Fahrenheit a Celsius",
 				"De Fahrenheit a Kelvin", "De Kelvin a Celsius", "De Kelvin a Fahrenheit" };
-		Object selectedTemperature = JOptionPane.showInputDialog(null, "Escolha a temperatura para qual você deseja converter", "Moedas",JOptionPane.PLAIN_MESSAGE, null, temperatures, temperatures[0]).toString();
+		String selectedTemperature = JOptionPane.showInputDialog(null, "Escolha a escala termométrica para qual você deseja converter", "Escalas Termométricas", JOptionPane.PLAIN_MESSAGE,
+				null, temperatures, temperatures[0]).toString();
+		
+		Locale.setDefault(Locale.US);
+		TemperatureConverter tc = new TemperatureConverter();
+		switch(selectedTemperature) {
+		case "De Celsius a Fahrenheit":
+			tc.celsiusToFahrenheit();
+			proceed();
+			break;
+			
+		case "De Celsius a Kelvin":
+			tc.celsiusToKelvin();
+			proceed();
+			break;
+			
+		case "De Fahrenheit a Celsius":
+			tc.fahrenheitToCelsius();
+			proceed();
+			break;
+			
+		case "De Fahrenheit a Kelvin":
+			tc.fahrenheitToKelvin();
+			proceed();
+			break;
+			
+		case "De Kelvin a Celsius":
+			tc.kelvinToCelsius();
+			proceed();
+			break;
+			
+		case "De Kelvin a Fahrenheit":
+			tc.kelvinToFahrenheit();
+			proceed();
+			break;
+		}
+	}
+	
+	public static void proceed() {
+		int check = JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Select an Option", JOptionPane.YES_NO_OPTION);
+		if(check == JOptionPane.YES_OPTION) {
+			menu();
+		}else {
+			JOptionPane.showMessageDialog(null,"Programa finalizado.");
+			System.exit(0);
+		}
 	}
 }
---
+++ src/application/TemperatureConverter.java
@@ -0,0 +1,45 @@
+package application;
+
+import javax.swing.JOptionPane;
+
+public class TemperatureConverter {
+	
+	String input = JOptionPane.showInputDialog("Insira um valor de temperatura para converter");
+	double receivedTemperature = Double.parseDouble(input);	
+	
+	public double celsiusToFahrenheit() {
+		double convertedTemperature = (receivedTemperature * 1.8) + 32;
+		JOptionPane.showMessageDialog(null,receivedTemperature + " °C são " + String.format("%.1f", convertedTemperature) + " °F.");
+		return convertedTemperature;
+	}
+	
+	public double celsiusToKelvin() {
+		double convertedTemperature = receivedTemperature + 273.15;
+		JOptionPane.showMessageDialog(null,receivedTemperature + " °C são " + String.format("%.1f", convertedTemperature) + " K.");
+		return convertedTemperature;
+	}
+	
+	public double fahrenheitToCelsius() {
+		double convertedTemperature = (receivedTemperature - 32) / 1.8;
+		JOptionPane.showMessageDialog(null,receivedTemperature + " °F são " + String.format("%.1f", convertedTemperature) + " °C.");
+		return convertedTemperature;
+	}
+	
+	public double fahrenheitToKelvin() {
+		double convertedTemperature = (receivedTemperature - 32) * 5 / 9 + 273.15;
+		JOptionPane.showMessageDialog(null,receivedTemperature + " °F são " + String.format("%.1f", convertedTemperature) + " K.");
+		return convertedTemperature;
+	}
+	
+	public double kelvinToCelsius() {
+		double convertedTemperature = receivedTemperature - 273.15;
+		JOptionPane.showMessageDialog(null,receivedTemperature + " K são " + String.format("%.1f", convertedTemperature) + " °C.");
+		return convertedTemperature;
+	}
+	
+	public double kelvinToFahrenheit() {
+		double convertedTemperature = (receivedTemperature - 273.15) * 1.8 + 32;
+		JOptionPane.showMessageDialog(null,receivedTemperature + " K são " + String.format("%.1f", convertedTemperature) + " °C.");
+		return convertedTemperature;
+	}
+}
